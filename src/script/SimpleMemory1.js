e878a9ed1d276fdcd629943ae02582e044dafc12
SongHaolong <48611936+NOIP-shl@users.noreply.github.com>
2019-08-20 09:19:19 +0800

    Update and rename SimpleMemory.js to SimpleMemory1.js

--- src/script/SimpleMemory1.js
+++ src/script/SimpleMemory1.js
@@ -154,12 +154,12 @@ if(initCheck()) {
 			animateSections: true
 		},
 		homeTopImg: [
-			"https://raw.githubusercontent.com/NOIP-shl/Cnblogs-Theme-SimpleMemory/master/img/home_top_bg.jpg"
+			"https://raw.githubusercontent.com/NOIP-shl/Cnblogs-Theme-SimpleMemory/master/img/mountain.jpg"
 		],
 		homeBannerText: "自强不息，厚德载物",
 		homeBannerTextType: "",
 		essayTopImg: [
-			"https://raw.githubusercontent.com/NOIP-shl/Cnblogs-Theme-SimpleMemory/master/img/nothome_top_bg.jpg"
+			"https://raw.githubusercontent.com/NOIP-shl/Cnblogs-Theme-SimpleMemory/master/img/mountain-1149897__340.jpg"
 		],
 		essayCodeHighlightingType: 'cnblogs',
 		essayCodeHighlighting: '',
